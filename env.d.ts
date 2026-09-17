b640e5af6c58b71264496940bd5de52740839791
francis.fuentes <francis.fuentes@ims.com.ph>
2026-09-16 21:34:28 -0700

    update .env variable

--- env.d.ts
+++ env.d.ts
@@ -5,4 +5,5 @@ declare const __APP_VERSION__: string
 interface ImportMetaEnv {
   readonly VITE_APP_PER_PAGE?: string
   readonly VITE_APP_MAINTENANCE_MODE?: string
+  readonly VITE_APP_VOUCHER_COMPANY_NAME?: string
 }
